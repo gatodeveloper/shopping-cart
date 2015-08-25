1534a2f281b8ca60f4c1b79ae6c4fedf80dad1dd
Diego Santiesteban <gatodeveloper@gmail.com>
2015-08-24 22:08:47 -0500

    update module products

--- shopping-cart/api/products/model.js
+++ shopping-cart/api/products/model.js
@@ -18,6 +18,16 @@ var ProductSchema = new Schema({
 	,	stockAmount: {type: Number }
 	,	photos: [{type: Object }]
 	,	taxes: [{type: Object }]
+	,	status: { type: String }
+	,	created: { type: Date, default: Date.now }
+});
+
+var ShoppingCartSchema = new Schema({
+		session: { type: String }
+	,	user: { type: String , unique: true }
+	,	products: [{ type: ObjectId, ref: 'Product'}]
+	,	created: { type: Date, default: Date.now }
 });
 
 var Product = mongoose.model('Product', ProductSchema);
+var ShoppingCart = mongoose.model('ShoppingCart', ShoppingCartSchema);
--- shopping-cart/api/users/controller.js
+++ shopping-cart/api/users/controller.js
@@ -6,7 +6,5 @@ var u = require('../utils');
 var mongoose = require("mongoose");
 var async = require('async');
 var _ = require('lodash');
-//var mailing = require("../mailing/controller");
-//var gm = require('gm').subClass({ imageMagick: true });
 var path = require('path');    
 
