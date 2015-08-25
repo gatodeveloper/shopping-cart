1dea128908e042300aa5cd6cca873135cf2196c0
Diego Santiesteban <gatodeveloper@gmail.com>
2015-08-24 19:54:04 -0500

    product properties

--- shopping-cart/api/products/model.js
+++ shopping-cart/api/products/model.js
@@ -11,5 +11,13 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var ObjectId = Schema.ObjectId;
 
-var ProductSchema = new Schema({});
+var ProductSchema = new Schema({
+		title: {type: String }
+	,	description: {type: String }
+	,	price: {type: Number }
+	,	stockAmount: {type: Number }
+	,	photos: [{type: Object }]
+	,	taxes: [{type: Object }]
+});
+
 var Product = mongoose.model('Product', ProductSchema);
